ebf19f7c863e0ee374c885f8a5bd044cfdf909d5
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-08 20:45:20 -0500

    fix errors

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,21 +2,27 @@ import { Component } from "react";
 
 export class Profile extends Component {
   state = {
-    results: null,
+    person: null,
   };
 
   async componentDidMount() {
     const url = "https://api.randomuser.me/";
     const result = await fetch(url);
     const data = await result.json();
-    this.setState({ results: data.results[0] });
+    console.log(data);
+    this.setState({ person: data.results[0] });
   }
   render() {
     return (
       <div>
-        <p>{this.state.gender}</p>
-        {/* <img src={this.state.pictire.medium} alt="profile pic" /> */}
-        <p>{this.state.cell}</p>
+        {!this.state.person ? (
+          <p>Loading....</p>
+        ) : (
+          <div>
+            <p>{this.state.person.name.first}</p>
+            <p>{this.state.person.name.last}</p>
+          </div>
+        )}
       </div>
     );
   }
